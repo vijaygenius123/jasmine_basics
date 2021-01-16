4511db623a8478b5aad3b76d7c2f4e64fe93b8b2
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2021-01-16 07:13:04 +0530

    Added test for get fullName

--- main.js
+++ main.js
@@ -1,26 +1,50 @@
 
-class User{
-    firstName;
-    middleName;
-    lastName;
 
-    constructor(data ={}) {
-        this.firstName = data.firstName || '';
-        this.middleName = data.middleName || '';
-        this.lastName = data.lastName || '';
-    }
-
-}
-
+/* Unit Testing: It Test Challenge
+*
+*   1. Write a unit test for lastName and middleName to test its default values.
+*   2. Group all 3 unit tests with an appropriate describe.
+*/
+/* Unit Testing: Setting Up Data with beforeEach Challenge
+*
+* 1. Add a new describe for the fullName
+* 2. Fully test the fullName get
+* 3. Use a nested beforeEach
+*/
 
 describe(`${User.name} Class`, function () {
-    it('should default firstName to empty ', function () {
-        //arrange
-        const data = {firstName: null};
-        //act
-        const model = new User(data);
-        //assert
-        expect(model.firstName).toBe('');
+
+    describe('Default Values', function () {
+        let model
+        beforeEach(() => {
+            const data = {}
+            model = new User(data);
+        })
+        it('should default firstName to empty ', function () {
+            expect(model.firstName).toBe('');
+        });
+
+        it('should default middleName to empty ', function () {
+            expect(model.middleName).toBe('');
+        });
+
+        it('should default lastName to empty ', function () {
+            expect(model.lastName).toBe('');
+        });
     });
+
+    describe('get fullName', function () {
+
+        it('should return only first and last name if middleName is empty', function () {
+            const model = new User({firstName: 'Vijayaraghavan', lastName: 'Sundararaman'})
+            expect(model.fullName).toBe('Vijayaraghavan Sundararaman');
+        });
+
+        it('should return first middle and last name if middleName exists', function () {
+            const model = new User({firstName: 'Rock', middleName: 'Dwayne', lastName: 'Johnson'})
+            expect(model.fullName).toBe('Rock D. Johnson');
+        });
+
+    })
 
 });
---
+++ user.js
@@ -0,0 +1,20 @@
+
+class User{
+    firstName;
+    middleName;
+    lastName;
+
+    constructor(data ={}) {
+        this.firstName = data.firstName || '';
+        this.middleName = data.middleName || '';
+        this.lastName = data.lastName || '';
+    }
+
+    get fullName(){
+        if(this.middleName.length > 0){
+            return `${this.firstName} ${this.middleName[0]}. ${this.lastName}`;
+        } else {
+            return `${this.firstName} ${this.lastName}`;
+        }
+    }
+}
